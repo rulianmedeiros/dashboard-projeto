2978e6fd6be3560475f327c1becd7d9564ef909a
rulianmedeiros <rulianm52@gmail.com>
2026-02-05 18:10:56 -0300

    FIX: Adicionando arquivos base do Vite para corrigir 404

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
+  base: './', // <-- Adicione esta linha! Ela corrige o erro 404.
   server: {
+    host: '0.0.0.0',
     port: 5173,
-    host: true
   }
 })
