09e4d31863f364a544eeb0b6cc270d086f2d2e5b
rulianmedeiros <rulianm52@gmail.com>
2026-02-05 21:57:39 -0300

    FIX: Build final com caminhos corrigidos

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react( )],
-  base: './', // <-- Adicione esta linha! Ela corrige o erro 404.
+  base: '/', // <-- Adicione esta linha! Ela corrige o erro 404.
   server: {
     host: '0.0.0.0',
     port: 5173,
